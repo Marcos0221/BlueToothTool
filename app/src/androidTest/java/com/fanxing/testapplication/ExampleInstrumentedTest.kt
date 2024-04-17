8604128fbeb9933569f02ff005dcadcf953904ba
繁星 <3091977221@qq.com>
2024-04-17 09:03:33 +0800

    编写测试方法

--- app/src/androidTest/java/com/fanxing/testapplication/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/fanxing/testapplication/ExampleInstrumentedTest.kt
@@ -1,7 +1,9 @@
 package com.fanxing.testapplication
 
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.fanxing.testapplication.utils.BlueToothManager
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -17,8 +19,19 @@ import org.junit.Assert.*
 class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
+        val TAG: String = "蓝牙设备状态测试"
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.fanxing.testapplication", appContext.packageName)
+
+        val blueToothManager = BlueToothManager(appContext)
+        var blueToothStatus = blueToothManager.getBlueToothStatus()
+
+            if (blueToothStatus == -1) {
+                Log.i(TAG, "useAppContext: 当前设备不支持蓝牙")
+            } else if (blueToothStatus == 0) {
+                Log.i(TAG, "useAppContext: 蓝牙未开启")
+            } else {
+                Log.i(TAG, "useAppContext: 蓝牙已开启")
+            }
     }
 }
