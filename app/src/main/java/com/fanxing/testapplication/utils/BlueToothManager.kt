031cec6a2742719b95dab97cc61c4681a528c763
繁星 <3091977221@qq.com>
2024-04-21 10:11:19 +0800

    优化条件

--- app/src/main/java/com/fanxing/testapplication/utils/BlueToothManager.kt
+++ app/src/main/java/com/fanxing/testapplication/utils/BlueToothManager.kt
@@ -24,11 +24,6 @@ class BlueToothManager(private val context: Context, private val bluetoothAdapte
      * @return -1: 当前设备不支持蓝牙; 0: 未开启蓝牙; 1: 已开启蓝牙;
      */
     fun getBlueToothStatus(): Int{
-        if (bluetoothAdapter == null){
-            // 设备不支持蓝牙
-            if (DEBUG) println("设备不支持蓝牙")
-            return -1
-        }
         if (!bluetoothAdapter.isEnabled) {
             // 蓝牙未开启
             if (DEBUG) println("蓝牙未开启")
