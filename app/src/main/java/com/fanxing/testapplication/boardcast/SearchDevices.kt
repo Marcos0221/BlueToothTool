b46e6b188919b20b9a68c2033596f330daa87577
繁星 <3091977221@qq.com>
2024-05-05 13:56:57 +0800

    修复错误写法

--- app/src/main/java/com/fanxing/testapplication/boardcast/SearchDevices.kt
+++ app/src/main/java/com/fanxing/testapplication/boardcast/SearchDevices.kt
@@ -1,16 +1,24 @@
 package com.fanxing.testapplication.boardcast
 
+import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.DebugUtils
 import android.util.Log
 
 class SearchDevices: BroadcastReceiver() {
     
     private val TAG: String = "SearchDevices"
+    var DeviceList: ArrayList<String> = ArrayList()
 
+    fun getInstance(): SearchDevices {
+        return this
+    }
+
+    @SuppressLint("MissingPermission")
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
             BluetoothAdapter.ACTION_STATE_CHANGED -> {
@@ -23,7 +31,13 @@ class SearchDevices: BroadcastReceiver() {
                 Log.i(TAG, "onReceive: 开始扫描")
             }
             BluetoothDevice.ACTION_FOUND -> {
-                Log.i(TAG, "onReceive: 得到设备对象")
+                var device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                val str = device?.name + "|" + device?.address
+                if (DeviceList.indexOf(str) == -1){
+                    if(device?.name!=null){
+                        Log.i(TAG, "onReceive: $str")
+                    }
+                }
             }
         }
     }
