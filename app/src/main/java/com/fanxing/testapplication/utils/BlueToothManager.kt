13c9db12354af1134229d3170893a20d5a489a98
繁星 <3091977221@qq.com>
2024-05-07 17:08:21 +0800

    *

--- app/src/main/java/com/fanxing/testapplication/utils/BlueToothManager.kt
+++ app/src/main/java/com/fanxing/testapplication/utils/BlueToothManager.kt
@@ -2,22 +2,40 @@ package com.fanxing.testapplication.utils
 
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
 import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import com.fanxing.testapplication.boardcast.SearchDevices
 
 class BlueToothManager(private val context: Context, private val bluetoothAdapter: BluetoothAdapter) {
 
+    private val TAG:String = "BlueToothManager"
+
     private val DEBUG: Boolean = true
+    val search: SearchDevices = SearchDevices().getInstance()
+
+    fun getBlueToothDeviceList(): ArrayList<String> {
+        Log.i(TAG, "getBlueToothDeviceList: ${search.DeviceList.toString()}")
+        return search.DeviceList
+    }
 
     /**
      * 扫描蓝牙设备
      */
     @SuppressLint("MissingPermission")
     fun ScanBlueToothDeviceList() {
-        if (bluetoothAdapter.isDiscovering) {
-            bluetoothAdapter.cancelDiscovery()
+        val statusCode = getBlueToothStatus()
+        if (statusCode == -1){
+            Toast.makeText(context, "当前设备不支持蓝牙", Toast.LENGTH_SHORT).show()
+        }else if (statusCode == 0){
+            Toast.makeText(context, "蓝牙已关闭，请手动打开蓝牙", Toast.LENGTH_SHORT).show()
+        }else{
+            if (bluetoothAdapter.isDiscovering) {
+                bluetoothAdapter.cancelDiscovery()
+            }
+            bluetoothAdapter.startDiscovery()
+            Toast.makeText(context, "开始扫描", Toast.LENGTH_SHORT).show()
         }
-        bluetoothAdapter.startDiscovery()
     }
 
     /**
